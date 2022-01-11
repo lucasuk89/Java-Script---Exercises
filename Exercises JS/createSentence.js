2285614b3e3e4d77d3298c840c2d4235a891d698
Lucas Ferraz Martins <lucasuk89@gmail.com>
2022-01-11 11:24:12 +0000

    atualização no exercicio createSentence.js

--- Exercises JS/createSentence.js
+++ Exercises JS/createSentence.js
@@ -1,7 +1,7 @@
-const person ={
-   name: "Lucas",
-   age:  32,
-   job: "Software Eng.",
+const person = {
+    name: "Lucas",
+    age: 32,
+    job: "Software Eng.",
 };
 
 const personTwo = {
@@ -10,10 +10,10 @@ const personTwo = {
     job: "Software Eng. 2"
 }
 
-function createSentence({name, age, job}){
+function createSentence({ name, age, job }) {
     //const {name, age, job} = person;
-    return  "Hello my name is " + name +  ", I am " + age + " and I am a " + job;
+    return "Hello my name is " + name + ", I am " + age + " and I am a " + job;
 };
 
- console.log(createSentence(person));
- console.log(createSentence(personTwo));
+console.log(createSentence(person));
+console.log(createSentence(personTwo));
